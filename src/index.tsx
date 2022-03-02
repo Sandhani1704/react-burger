1046e1b570f3338ff759d0b620d504dc7785600a
Anna Galkina <sandhani1704@yandex.ru>
2022-03-03 00:07:48 +0300

    fix: add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/app";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import { store } from "./services/store";
+
+const isProduction = process.env.NODE_ENV === 'production';
+const Router: typeof HashRouter = isProduction ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
   <React.StrictMode>
