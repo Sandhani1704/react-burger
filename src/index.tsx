6e24794adef9be174d5f5c7790127eaaf8d93495
Anna Galkina <sandhani1704@yandex.ru>
2022-03-03 21:19:11 +0300

    fix: delete HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/app";
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+//import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store } from "./services/store";
 
-const isProduction = process.env.NODE_ENV === 'production';
-const Router: typeof HashRouter = isProduction ? HashRouter : BrowserRouter;
+// const isProduction = process.env.NODE_ENV === 'production';
+// const Router: typeof HashRouter = isProduction ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename="/react-burger">
+      <Router basename="react-burger">
       <App />
       </Router>
     </Provider>
